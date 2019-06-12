04f9f8536f947ce79d61812484317f7263610873
fida <alias_rafidah@yahoo.com>
2019-06-12 17:24:14 +0800

    update models

---
+++ server.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+from models import db
+app = Flask(__name__)
+
+
+@app.before_request("/")
+def before_request():
+    db.connect()
+
+
+@app.after_request
+def after_request():
+    db.close()
+    return response
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
